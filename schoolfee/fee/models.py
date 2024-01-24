4e78efdb5ec5eb0fb11752f96d792b9747a6a110
smagucha <smagucha@gmail.com>
2024-01-24 23:25:37 +0300

    added student model

--- schoolfee/fee/models.py
+++ schoolfee/fee/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Student(models.Model):
+    FirstName = models.CharField(max_length=100)
+    LastName = models.CharField(max_length=100)
+    DateOfBirth = models.DateField()
+    Grade = models.CharField(max_length=100)
+    GuardianorparentContactInfo = models.CharField(max_length=100)
