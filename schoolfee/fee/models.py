4cc852b9aad2c7be780e4c8f9f0bcc20a2e1f20c
smagucha <smagucha@gmail.com>
2024-01-29 23:19:00 +0300

    added fee table

--- schoolfee/fee/models.py
+++ schoolfee/fee/models.py
@@ -15,3 +15,15 @@ class Student(models.Model):
 
     def __str__(self):
         return "%s %s" % (self.FirstName, self.LastName)
+
+
+class Fee(models.Model):
+    fee = models.CharField(max_length=100, unique=True)
+    description = models.TextField()
+    Amount = models.PostiveIntegerField()
+    DueDate = models.DateField()
+    Grade = models.CharField(max_length=100)
+
+    def __str__(self):
+        eturn "%s" % (self.fee)
+
