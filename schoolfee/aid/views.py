d7d42c67373debdc783119dbaf987e0fdc721218
smagucha <smagucha@gmail.com>
2024-02-04 22:14:05 +0300

    modified aidfee view

--- schoolfee/aid/views.py
+++ schoolfee/aid/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView, DeleteView
+from django.views.generic.list import ListView
+from .models import AidName, FeeAid
+from django.urls import reverse_lazy
 
-# Create your views here.
+# FeeAid
+
+
+class CreateAid(CreateView):
+    model = AidName
+    fields = "__all__"
+    success_url = reverse_lazy("allstudent")
+
+
+class ListAid(ListView):
+    model = AidName
+
+
+class DeleteAid(DeleteView):
+    model = AidName
+    success_url = reverse_lazy("allstudent")
+
+
+class CreateFeeAid(CreateView):
+    model = FeeAid
+    fields = "__all__"
+
+
+class ListFeeAid(ListView):
+    model = FeeAid
+
+
+class DeleteAid(DeleteView):
+    model = FeeAid
+    success_url = reverse_lazy("allstudent")
